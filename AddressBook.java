b38e2e58ce8005063aca7693e3d78e68cd73914a
shirishdh15 <121274610+shirishdh15@users.noreply.github.com>
2023-02-06 19:27:35 +0530

    Add files via upload

--- AddressBook.java
+++ AddressBook.java
@@ -11,4 +11,34 @@ import java.util.Scanner;
         Contact person = new Contact();
         ArrayList<Contact> people = new ArrayList<Contact>();
         Scanner sc = new Scanner(System.in);
+
+        public void addContact() {
+
+            System.out.println("First Name :");
+            person.firstName = sc.nextLine();
+
+            System.out.println("Last Name :");
+            person.LastName = sc.nextLine();
+
+            System.out.println("Address :");
+            person.address = sc.nextLine();
+
+            System.out.println("City :");
+            person.city = sc.nextLine();
+
+            System.out.println("State :");
+            person.state = sc.nextLine();
+
+            System.out.println("Zip :");
+            person.Zip = sc.nextLine();
+
+            System.out.println("Phone Number :");
+            person.phoneNumber = sc.nextLine();
+
+            System.out.println("Email :");
+            person.email = sc.nextLine();
+
+            people.add(person);
+        }
+
     }
