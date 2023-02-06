79e340a6703a60202556994cd5debbc5dc84d8f6
shirishdh15 <121274610+shirishdh15@users.noreply.github.com>
2023-02-06 19:15:59 +0530

    Add files via upload

---
+++ AddressBook.java
@@ -0,0 +1,14 @@
+package Day9;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+    public class AddressBook {
+        public static void main(String[] agrs) {
+            System.out.println("Address Book");
+        }
+
+        Contact person = new Contact();
+        ArrayList<Contact> people = new ArrayList<Contact>();
+        Scanner sc = new Scanner(System.in);
+    }
