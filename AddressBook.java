176101b03c0b66cd4e16c8190e86f1b5652ccca8
shirishdh15 <121274610+shirishdh15@users.noreply.github.com>
2023-02-06 19:32:43 +0530

    Add files via upload

--- AddressBook.java
+++ AddressBook.java
@@ -40,5 +40,89 @@ import java.util.Scanner;
 
             people.add(person);
         }
+        public void editContact()
+        {
+
+            System.out.println("Enter the person whose contact to be edited :");
+            System.out.println("Enter First Name:");
+            String fname = sc.nextLine();
+            System.out.println("Enter Last Name:");
+            String lname = sc.nextLine();
+
+            int flag = 0;
+            for (int j = 0; j < people.size(); j++)
+            {
+                Contact person = people.get(j);
+                if (person.firstName.equals(fname) && person.LastName.equals(lname))
+                {
+                    flag = 1;
+                    System.out.println("First Name : " + person.firstName);
+                    System.out.println("Last Name  : " + person.LastName);
+                    System.out.println("Address    : " + person.address);
+                    System.out.println("City       : " + person.city);
+                    System.out.println("State      : " + person.state);
+                    System.out.println("Zip        : " + person.Zip);
+                    System.out.println("Phone Number:" + person.phoneNumber);
+                    System.out.println("Email      : " + person.email);
+                    System.out.println("---------------------------------------");
+                    System.out.println("Enter the number which you want to edit\n1.First Name\n2.Last Name\n3.Address\n4.C\n5.State\n6.Zip\n7.Phone Number\n8.Email");
+                    int choose = sc.nextInt();
+                    switch (choose)
+                    {
+                        case 1:
+                            System.out.println("first name:");
+                            Scanner sc = new Scanner(System.in);
+                            person.firstName =  sc.nextLine();
+                            System.out.println("edited succesfully");
+                            break;
+                        case 2:
+                            System.out.println("last name:");
+                            Scanner scanner = new Scanner(System.in);
+                            person.LastName = scanner.nextLine();
+                            System.out.println("edited succesfully");
+                            break;
+                        case 3:
+                            System.out.println("address:");
+                            Scanner s = new Scanner(System.in);
+                            person.address = s.nextLine();
+                            System.out.println("edited succesfully");
+                            break;
+
+                        case 4:
+                            System.out.println("city:");
+                            Scanner scan = new Scanner(System.in);
+                            person.city = scan.nextLine();
+                            System.out.println("edited succesfully");
+                            break;
+                        case 5:
+                            System.out.println("state:");
+                            Scanner userInput = new Scanner(System.in);
+                            person.state = userInput.nextLine();
+                            System.out.println("edited succesfully");
+                            break;
+                        case 6:
+                            System.out.println("zip:");
+                            Scanner user = new Scanner(System.in);
+                            person.Zip = user.nextLine();
+                            System.out.println("edited succesfully");
+                            break;
+                        case 7:
+                            System.out.println("phone_number:");
+                            Scanner input = new Scanner(System.in);
+                            person.phoneNumber = input.nextLine();
+                            System.out.println("edited succesfully");
+                            break;
+                        case 8:
+                            System.out.println("email:");
+                            Scanner in = new Scanner(System.in);
+                            person.email = in.nextLine();
+                            System.out.println("edited succesfully");
+                            break;
+                    }
+                }
+            }
+            if (flag == 0)
+                System.out.println("Contact not found!!!");
+        }
 
     }
